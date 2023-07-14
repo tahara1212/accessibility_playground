7b6eef9add54bc149af48ebeab4ce041f74303cf
shumpei.tahara <shumpei.tahara@nijibox.co.jp>
2023-07-14 10:40:54 +0900

    chakra ui 導入

---
+++ src/const/navigation.ts
@@ -0,0 +1,30 @@
+export const NAVIGATION = [
+  {
+    CHAPTER: "Webアクセシビリティとは",
+    LIST: [
+      {
+        TITLE: "アクセシビリティとは",
+      },
+      {
+        TITLE: "WCAG",
+      },
+      {
+        TITLE: "Webアクセシビリティに取り組む理由",
+      },
+    ],
+  },
+  {
+    CHAPTER: "Webアクセシビリティの基礎",
+    LIST: [
+      {
+        TITLE: "基礎となるマシンリーダブルを理解する",
+      },
+      {
+        TITLE: "セマンティクスを補完するWAI-ARIA",
+      },
+      {
+        TITLE: "キーボード操作の基本",
+      },
+    ],
+  },
+];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
+// 2. custom colorやfontなどで theme を拡張する
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
